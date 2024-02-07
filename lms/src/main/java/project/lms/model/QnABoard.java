4d01d17cfa5449961132ea88dca0663cb71920ad
iridescentu <jihee.yoon.119@gmail.com>
2024-02-07 09:40:40 +0900

    QnA Reply Table Structure

--- lms/src/main/java/project/lms/model/QnABoard.java
+++ lms/src/main/java/project/lms/model/QnABoard.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
